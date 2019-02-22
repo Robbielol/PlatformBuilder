94f34ebe3418a1db6d9b703b0e740493f43c4cca
Brian950 <brianjohnheaphy@gmail.com>
2019-02-22 15:37:24 +0000

    Added new resource folder.

--- core/src/com/gui/constructor/ConstructorScreen.java
+++ core/src/com/gui/constructor/ConstructorScreen.java
@@ -7,9 +7,9 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.mygdx.game.PlatformBuilder;
 
@@ -21,6 +21,11 @@ public class ConstructorScreen implements Screen {
     private Label framesLabel;
     private Stage stage;
     private Table table;
+    private TextButton rectButton;
+    private TextButton triButton;
+    private TextButton circButton;
+    private TextureAtlas buttonAtlas;
+    private Skin skin;
     private float frameRate;
     private float sinceChange;
     private long lastTimeCounted;
@@ -32,6 +37,9 @@ public class ConstructorScreen implements Screen {
 
     public void create(){
         batch = new SpriteBatch();
+        buttonAtlas = new TextureAtlas(Gdx.files.internal("quantum/skin/quantum-horizon-ui.atlas"));
+        skin = new Skin();
+        skin.addRegions(buttonAtlas);
 
         // FPS counter
         frames = new BitmapFont();
@@ -40,8 +48,46 @@ public class ConstructorScreen implements Screen {
         framesLabel = new Label("FPS: "+frameRate, new Label.LabelStyle(frames, Color.BLACK));
         framesLabel.setPosition(Gdx.graphics.getWidth()/50, Gdx.graphics.getHeight()-20);
 
+        // Buttons
+        TextButton.TextButtonStyle rectButtonStyle = new TextButton.TextButtonStyle();
+        rectButtonStyle.up = skin.getDrawable("button");
+        rectButtonStyle.down = skin.getDrawable("button-pressed");
+        rectButtonStyle.font = new BitmapFont(Gdx.files.internal("quantum/skin/font-export.fnt"));
+        rectButton = new TextButton("Rectangle", rectButtonStyle);
+        rectButton.setTransform(true);
+        rectButton.setColor(new Color(4555));
+
+        TextButton.TextButtonStyle triButtonStyle = new TextButton.TextButtonStyle();
+        triButtonStyle.up = skin.getDrawable("button");
+        triButtonStyle.down = skin.getDrawable("button-pressed");
+        triButtonStyle.font = new BitmapFont(Gdx.files.internal("quantum/skin/font-export.fnt"));
+        triButton = new TextButton("Triangle", triButtonStyle);
+        triButton.setTransform(true);
+        triButton.setColor(new Color(4555));
+
+        TextButton.TextButtonStyle circButtonStyle = new TextButton.TextButtonStyle();
+        circButtonStyle.up = skin.getDrawable("button");
+        circButtonStyle.down = skin.getDrawable("button-pressed");
+        circButtonStyle.font = new BitmapFont(Gdx.files.internal("quantum/skin/font-export.fnt"));
+        circButton = new TextButton("Circle", circButtonStyle);
+        circButton.setTransform(true);
+        circButton.setColor(new Color(4555));
+
+        // Table
+        table = new Table();
+        table.add(rectButton).left();
+        table.row();
+        table.add(triButton).left();
+        table.row();
+        table.add(circButton).left();
+        BackgroundColor bgCol = new BackgroundColor("images/window2.png");
+        table.setBackground(bgCol);
+        table.setPosition(Gdx.graphics.getWidth()/8, Gdx.graphics.getHeight()-120);
+
         // Stage
         stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        stage.addActor(table);
         stage.addActor(framesLabel);
     }
 
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,12 +3,15 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.CharacterTest;
+import com.mygdx.game.PlatformBuilder;
 
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		//config.useGL30 = true;
-		new LwjglApplication(new CharacterTest(), config);
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.useGL30 = true;
+		cfg.height = 600;
+		cfg.width = 900;
+		new LwjglApplication(new PlatformBuilder(), cfg);
 	}
 }
